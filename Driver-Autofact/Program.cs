baee6b1efb325a8c12c48f0a9cd47a8a06819164
Sinan.Nar <Sinan.Nar@soeidental.com>
2019-05-10 17:39:44 +1200

    autofac used

---
+++ Driver-Autofact/Program.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Service;
+using Service.Budget;
+using Service.Contract;
+using Service.Gaming;
+using System;
+
+namespace Driver_Autofact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<AliExpressMemory>().As<IPrimaryMemory<IBudget>>();
+            builder.RegisterType<KingstonMemory>().As<IPrimaryMemory<IGaming>>();
+
+            builder.RegisterType<AcerMotherboard>().As<IMotherboard<IBudget>>();
+            builder.RegisterType<AsusMotherboard>().As<IMotherboard<IGaming>>();
+
+            builder.RegisterType<AliBabaMemory>().As<ISecondaryMemory<IBudget>>();
+            builder.RegisterType<Sandisk>().As<ISecondaryMemory<IGaming>>();
+
+            builder.RegisterType<IntelI3>().As<IProcessor<IBudget>>();
+            builder.RegisterType<IntelI7>().As<IProcessor<IGaming>>();
+
+            builder.RegisterType<Computer<IBudget>>().As<IComputer<IBudget>>();
+            builder.RegisterType<Computer<IGaming>>().As<IComputer<IGaming>>();
+
+
+            var conter = builder.Build();
+
+            var budgetComputer = conter.Resolve<IComputer<IBudget>>();
+            var gamingComputer = conter.Resolve<IComputer<IGaming>>();
+
+            budgetComputer.FunctionCallExample();
+            gamingComputer.FunctionCallExample();
+        }
+    }
+}
--- Driver/Program.cs
+++ Driver/Program.cs
@@ -12,16 +12,22 @@ namespace Driver
         static void Main(string[] args)
         {
             var serviceProvider = new ServiceCollection()
+
                 .AddScoped(typeof(IPrimaryMemory<IBudget>), typeof(AliExpressMemory))
                 .AddScoped(typeof(IPrimaryMemory<IGaming>), typeof(KingstonMemory))
+                
                 .AddScoped(typeof(IMotherboard<IBudget>), typeof(AcerMotherboard))
                 .AddScoped(typeof(IMotherboard<IGaming>), typeof(AsusMotherboard))
+                
                 .AddScoped(typeof(ISecondaryMemory<IBudget>), typeof(AliBabaMemory))
                 .AddScoped(typeof(ISecondaryMemory<IGaming>), typeof(Sandisk))
+                
                 .AddScoped(typeof(IProcessor<IBudget>), typeof(IntelI3))
                 .AddScoped(typeof(IProcessor<IGaming>), typeof(IntelI7))
+                
                 .AddScoped(typeof(IComputer<IBudget>), typeof(Computer<IBudget>))
                 .AddScoped(typeof(IComputer<IGaming>), typeof(Computer<IGaming>))
+                
                 .BuildServiceProvider();
 
             var budgetComputer = serviceProvider.GetService<IComputer<IBudget>>();
